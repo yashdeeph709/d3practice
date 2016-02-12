64ef9d09e4b1b582351e153e808957890058e1ab
Yashdeep Hinge <yashdeeph709@gmail.com>
2016-02-12 16:26:57 +0530

    scale applied on x y and radius

--- script.js
+++ script.js
@@ -1,37 +1,61 @@
-var dataset = [
-                [5, 20,"india"], [480, 90,"pak"], [250, 50,"china"], [100, 33,"nepal"], [330, 95,"bhutan"],
-                [410, 12,"bangladesh"], [475, 44,"UK"], [25, 67,"USA"], [85, 21,"Parris"], [220, 88,"Sweden"]
-              ];
-var svgWidth=500,svgHeight=200;
-var svg=d3.select("body")
-		  .append("svg")
-		  .attr("width",svgWidth)
-		  .attr("height",svgHeight);
-svg.selectAll("circle")
-   .data(dataset)
-   .enter()
-   .append('circle')
-   .attr('cx',function(d){
-   		return d[0];
-   })
-   .attr('cy',function(d){
-   		return d[1];
-   })
-   .attr("r", function(d){return Math.sqrt(svgHeight - d[1]);})
-   .style('fill', 'green');
 
-svg.selectAll("text")
-.data(dataset)
-.enter()
-.append("text")
-.text(function(d){
-	return d[2];
-})
-.attr('font-size',10)
-.attr("fill","yellow")
-.attr('x',function(d){
-   		return d[0]-Math.sqrt(svgHeight-d[1]);
-})
-.attr('y',function(d){
-	return d[1]
-})
+
+      //Width and height
+      var w = 500;
+      var h = 300;
+      
+      var dataset = [
+              [5, 20], [480, 90], [250, 50], [100, 33], [330, 95],
+              [410, 12], [475, 44], [25, 67], [85, 21], [220, 88],[600,150]
+              ];
+var padding = 20;
+      //Create scale functions
+      var xScale = d3.scale.linear()
+                 .domain([0, d3.max(dataset, function(d) { return d[0]; })])
+                 .range([padding, w-padding*2]);
+
+      var yScale = d3.scale.linear()
+                 .domain([0, d3.max(dataset, function(d) { return d[1]; })])
+                 .range([h - padding, padding]);
+  
+      var rScale = d3.scale.linear()
+                     .domain([0, d3.max(dataset, function(d) { return d[1]; })])
+                     .range([2, 5]);
+      //Create SVG element
+      var svg = d3.select("body")
+            .append("svg")
+            .attr("width", w)
+            .attr("height", h);
+
+      svg.selectAll("circle")
+         .data(dataset)
+         .enter()
+         .append("circle")
+         .attr("cx", function(d) {
+            return xScale(d[0]);
+         })
+         .attr("cy", function(d) {
+            return yScale(d[1]);
+         })
+         .attr("r", function(d) {
+            return rScale(d[1]);
+         });
+
+      svg.selectAll("text")
+         .data(dataset)
+         .enter()
+         .append("text")
+         .text(function(d) {
+            return d[0] + "," + d[1];
+         })
+         .attr("x", function(d) {
+            return xScale(d[0]);
+         })
+         .attr("y", function(d) {
+            return yScale(d[1]);
+         })
+         .attr("font-family", "sans-serif")
+         .attr("font-size", "11px")
+         .attr("fill", "red");
+      
+    
